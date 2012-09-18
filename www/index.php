2ba0a514efa607b7b8bfade93bbc8ab8efe9a1f3
tlumley (Thomas Lumley) <thomas.lumley@r-project.org>
2012-09-18 22:33:16 +0000

    simple description
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sqlsurvey@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,9 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+  <p> This project has two packages: sqlsurvey, for analysis of large surveys, and RMonetDB, used by sqlsurvey to communicate with the MonetDB database </p>
+
+   <p> Both packages require <a href="http://www.monetdb.org/Downloads?x=144&y=18">MonetDB</a>. Under Windows it is important to use a 64-bit version. 
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
